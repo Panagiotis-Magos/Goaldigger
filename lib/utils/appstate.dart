c9705d0e868fb9eb5fb20557c6505d7cef735999
Panagiotis-Magos <160389166+Panagiotis-Magos@users.noreply.github.com>
2025-01-13 18:48:05 +0200

    Add files via upload

--- lib/utils/appstate.dart
+++ lib/utils/appstate.dart
@@ -2,6 +2,8 @@ class AppState {
   static final AppState _instance = AppState._internal();
   
   int globaluserId=-1; // To store the user's ID globally
+  String accessory ='assets/images/defaultac.png';
+  String theme = 'assets/images/defaultstyle.png';
 
   factory AppState() {
     return _instance;
